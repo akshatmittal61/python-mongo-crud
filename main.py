606b1e6053594ec81f3c85f18a32e626a59769c9
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-10 20:43:14 +0530

    added mock data

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from data import users
 
 app = FastAPI()
+
 
 @app.get('/')
 def get_root():
     return {
         'message': 'success',
         'data': 'Hello World'
+    }
+
+
+@app.get('/users')
+def get_users():
+    users_to_send = []
+    for user in users:
+        users_to_send.append({
+            'id': user['id'],
+            'name': user['name'],
+            'username': user['username'],
+            'email': user['email'],
+        })
+    return {
+        'message': 'success',
+        'data': users_to_send
     }
