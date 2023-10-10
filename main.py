75858832aa44b6adf7469b1b772a1afe0efbb088
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-11 00:45:56 +0530

    exceptional handling

--- main.py
+++ main.py
@@ -28,14 +28,32 @@ def get_users():
 def get_user_by_id(user_id, response: Response):
     res = None
     http = HTTP(response)
-    for user in users:
-        if str(user['id']) == user_id:
-            res = user_model_abstraction(user)
+    try:
+        user_id = int(user_id)
+        for user in users:
+            if user['id'] == user_id:
+                res = user_model_abstraction(user)
 
-    if res is None:
+        if res is None:
+            http.status(status.HTTP_404_NOT_FOUND)
+            return {
+                'message': 'failure',
+                'data': res
+            }
+
+        return {
+            'message': 'success',
+            'data': res
+        }
+    except ValueError as e:
         http.status(status.HTTP_404_NOT_FOUND)
-
-    return {
-        'message': 'success',
-        'data': res
-    }
+        return {
+            'message': 'failure',
+            'data': 'Invalid user id'
+        }
+    except Exception as e:
+        http.status(status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return {
+            'message': 'failure',
+            'data': str(e)
+        }
