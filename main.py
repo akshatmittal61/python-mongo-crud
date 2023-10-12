3256eff7d3373ba8d7f079e57a8cd3d8a425f99e
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-12 17:21:19 +0530

    created udpate user api

--- main.py
+++ main.py
@@ -63,3 +63,29 @@ def add_user(name: str, username: str, email: str, website: str, response: Respo
         return http.response(status.HTTP_201_CREATED, 'New User added successfully')
     except Exception as e:
         return http.response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
+
+
+@app.patch('/users/{user_id}')
+def update_user(user_id, data, response: Response):
+    http = HTTP(response)
+    try:
+        user_id = int(user_id)
+        data = eval(data)
+        if user_id not in [user['id'] for user in users]:
+            return http.response(status.HTTP_404_NOT_FOUND, 'User not found')
+        if len(data) == 0:
+            return http.response(status.HTTP_400_BAD_REQUEST, 'No details updated')
+        for user in users:
+            if user['id'] == user_id:
+                for key in ['name', 'email', 'username']:
+                    if key in data:
+                        user[key] = data[key]
+                break
+        return http.response(status.HTTP_200_OK, {
+            'message': 'User updated successfully',
+            'user': [user for user in users if user_id == user['id']][0]
+        })
+    except ValueError:
+        return http.response(status.HTTP_404_NOT_FOUND, 'Invalid user id')
+    except Exception as e:
+        return http.response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
